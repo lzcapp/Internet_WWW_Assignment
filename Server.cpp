542901fcdbbcd486429ee0408cf4ecdafa07e63b
Zichen Luo <iluo@163.com>
2019-03-23 22:09:32 +0800

    Update Server.cpp
    Chnage encoding to UTF-8

--- Server.cpp
+++ Server.cpp
@@ -1,18 +1,18 @@
 // SimpleHTTPServer.cpp
-// ���ܣ�ʵ�ּ򵥵�web���������ܣ���ͬʱ��Ӧ��������������
-//       1��������ļ����ڣ��������������ʾ���ļ���
-//       2������ļ������ڣ��򷵻�404-file not foundҳ��
-//       3��ֻ֧��GET��HEAD����
-// HTTP1.1 �� 1.0��ͬ��Ĭ���ǳ������ӵ�(keep-alive)
+// 功能：实现简单的web服务器功能，能同时响应多个浏览器的请求：
+//       1、如果该文件存在，则在浏览器上显示该文件；
+//       2、如果文件不存在，则返回404-file not found页面
+//       3、只支持GET、HEAD方法
+// HTTP1.1 与 1.0不同，默认是持续连接的(keep-alive)
 #pragma warning(disable:4996)
 #include <Winsock2.h>
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
-#include <direct.h>     // Ŀ¼ͷ�ļ�
+#include <direct.h>     // 目录头文件
 #pragma comment(lib,"Ws2_32.lib")
 
-// http Ĭ�϶˿���80�����80�˿ڱ�ռ����ô�ĸ��˿ڼ���
+// http 默认端口是80，如果80端口被占用那么改个端口即可
 #define DEFAULT_PORT 80 
 #define BUF_LENGTH 1024
 #define MIN_BUF 128
@@ -33,42 +33,42 @@ DWORD WINAPI SimpleHTTPServer(LPVOID lparam)
 	char path[_MAX_PATH];
 	int i, j;
 
-	// ������0��ÿ�β���ǰ��Ҫ�ǵ��建�棬����ϰ�ߣ�
-	// ����տ��ܳ��ֵ�����������롢��̨�������뻹������ͬ
-	// ԭ�򣺲���ջ�������� '\0'�ַ�Ϊֹ������ǰ��Ĳ���'\0' Ҳ��һ�����
+	// 缓存清0，每次操作前都要记得清缓存，养成习惯；
+	// 不清空可能出现的现象：输出乱码、换台机器乱码还各不相同
+	// 原因：不清空会输出遇到 '\0'字符为止，所以前面的不是'\0' 也会一起输出
 	memset(recv_buf, 0, sizeof(recv_buf));
-	if (recv(sAccept, recv_buf, sizeof(recv_buf), 0) == SOCKET_ERROR)   //���մ���
+	if (recv(sAccept, recv_buf, sizeof(recv_buf), 0) == SOCKET_ERROR)   //接收错误
 	{
 		printf("recv() Failed:%d\n", WSAGetLastError());
 		return USER_ERROR;
 	}
 	else
-		printf("recv data from client:%s\n", recv_buf); //���ճɹ�����ӡ������
+		printf("recv data from client:%s\n", recv_buf); //接收成功，打印请求报文
 
-	//������������
+	//处理接收数据
 	i = 0; j = 0;
-	// ȡ����һ�����ʣ�һ��ΪHEAD��GET��POST
+	// 取出第一个单词，一般为HEAD、GET、POST
 	while (!(' ' == recv_buf[j]) && (i < sizeof(method) - 1))
 	{
 		method[i] = recv_buf[j];
 		i++; j++;
 	}
-	method[i] = '\0';   // ������������Ҳ�ǳ�ѧ�ߺ����׺��ӵĵط�
+	method[i] = '\0';   // 结束符，这里也是初学者很容易忽视的地方
 
-	// �������GET��HEAD��������ֱ�ӶϿ���������
-	// ��������Ĺ淶Щ���Է��������һ��501δʵ�ֵı�ͷ��ҳ��
+	// 如果不是GET或HEAD方法，则直接断开本次连接
+	// 如果想做的规范些可以返回浏览器一个501未实现的报头和页面
 	if (stricmp(method, "GET") && stricmp(method, "HEAD"))
 	{
-		closesocket(sAccept); //�ͷ������׽��֣�������ÿͻ���ͨ��
+		closesocket(sAccept); //释放连接套接字，结束与该客户的通信
 		printf("not get or head method.\nclose ok.\n");
 		printf("***********************\n\n\n\n");
 		return USER_ERROR;
 	}
 	printf("method: %s\n", method);
 
-	// ��ȡ���ڶ�������(url�ļ�·�����ո����)������'/'��Ϊwindows�µ�·���ָ���'\'
-	// ����ֻ���Ǿ�̬����(����url�г���'?'��ʾ�Ǿ�̬����Ҫ����CGI�ű���'?'������ַ�����ʾ���������������'+'����
-	// ���磺www.csr.com/cgi_bin/cgi?arg1+arg2 �÷�����ʱҲ�в�ѯ�����ڳ���������)
+	// 提取出第二个单词(url文件路径，空格结束)，并把'/'改为windows下的路径分隔符'\'
+	// 这里只考虑静态请求(比如url中出现'?'表示非静态，需要调用CGI脚本，'?'后面的字符串表示参数，多个参数用'+'隔开
+	// 例如：www.csr.com/cgi_bin/cgi?arg1+arg2 该方法有时也叫查询，早期常用于搜索)
 	i = 0;
 	while ((' ' == recv_buf[j]) && (j < sizeof(recv_buf)))
 		j++;
@@ -85,39 +85,39 @@ DWORD WINAPI SimpleHTTPServer(LPVOID lparam)
 	url[i] = '\0';
 	printf("url: %s\n", url);
 
-	// �������url·��ת��Ϊ����·��
+	// 将请求的url路径转换为本地路径
 	_getcwd(path, _MAX_PATH);
 	strcat(path, url);
 	printf("path: %s\n", path);
 
-	// �򿪱���·���µ��ļ������紫������r�ı���ʽ�򿪻����
+	// 打开本地路径下的文件，网络传输中用r文本方式打开会出错
 	FILE *resource = fopen(path, "rb");
 
-	// û�и��ļ�����һ���򵥵�404-file not found��htmlҳ�棬���Ͽ���������
+	// 没有该文件则发送一个简单的404-file not found的html页面，并断开本次连接
 	if (resource == NULL)
 	{
 		file_not_found(sAccept);
-		// ���method��GET�������Զ����file not foundҳ��
+		// 如果method是GET，则发送自定义的file not found页面
 		if (0 == stricmp(method, "GET"))
 			send_not_found(sAccept);
 
-		closesocket(sAccept); //�ͷ������׽��֣�������ÿͻ���ͨ��
+		closesocket(sAccept); //释放连接套接字，结束与该客户的通信
 		printf("file not found.\nclose ok.\n");
 		printf("***********************\n\n\n\n");
 		return USER_ERROR;
 	}
 
-	// ����ļ����ȣ��ǵ������ļ�ָ�뵽�ļ�ͷ
+	// 求出文件长度，记得重置文件指针到文件头
 	fseek(resource, 0, SEEK_SET);
 	fseek(resource, 0, SEEK_END);
 	long flen = ftell(resource);
 	printf("file length: %ld\n", flen);
 	fseek(resource, 0, SEEK_SET);
 
-	// ����200 OK HEAD
+	// 发送200 OK HEAD
 	file_ok(sAccept, flen);
 
-	// �����GET���������������Դ
+	// 如果是GET方法则发送请求的资源
 	if (0 == stricmp(method, "GET"))
 	{
 		if (0 == send_file(sAccept, resource))
@@ -127,7 +127,7 @@ DWORD WINAPI SimpleHTTPServer(LPVOID lparam)
 	}
 	fclose(resource);
 
-	closesocket(sAccept); //�ͷ������׽��֣�������ÿͻ���ͨ��
+	closesocket(sAccept); //释放连接套接字，结束与该客户的通信
 	printf("close ok.\n");
 	printf("***********************\n\n\n\n");
 
@@ -135,7 +135,7 @@ DWORD WINAPI SimpleHTTPServer(LPVOID lparam)
 
 }
 
-// ����404 file_not_found��ͷ
+// 发送404 file_not_found报头
 int file_not_found(SOCKET sAccept)
 {
 	char send_buf[MIN_BUF];
@@ -156,7 +156,7 @@ int file_not_found(SOCKET sAccept)
 	return 0;
 }
 
-// ����200 ok��ͷ
+// 发送200 ok报头
 int file_ok(SOCKET sAccept, long flen)
 {
 	char send_buf[MIN_BUF];
@@ -179,7 +179,7 @@ int file_ok(SOCKET sAccept, long flen)
 	return 0;
 }
 
-// �����Զ����file_not_foundҳ��
+// 发送自定义的file_not_found页面
 int send_not_found(SOCKET sAccept)
 {
 	char send_buf[MIN_BUF];
@@ -192,13 +192,13 @@ int send_not_found(SOCKET sAccept)
 	return 0;
 }
 
-// �����������Դ
+// 发送请求的资源
 int send_file(SOCKET sAccept, FILE *resource)
 {
 	char send_buf[BUF_LENGTH];
 	while (1)
 	{
-		memset(send_buf, 0, sizeof(send_buf));       //������0
+		memset(send_buf, 0, sizeof(send_buf));       //缓存清0
 		fgets(send_buf, sizeof(send_buf), resource);
 		//  printf("send_buf: %s\n",send_buf);
 		if (SOCKET_ERROR == send(sAccept, send_buf, strlen(send_buf), 0))
@@ -214,23 +214,23 @@ int send_file(SOCKET sAccept, FILE *resource)
 int main()
 {
 	WSADATA wsaData;
-	SOCKET sListen, sAccept;        //�����������׽��֣������׽���
-	int serverport = DEFAULT_PORT;   //�������˿ں�
-	struct sockaddr_in ser, cli;   //��������ַ���ͻ��˵�ַ
+	SOCKET sListen, sAccept;        //服务器监听套接字，连接套接字
+	int serverport = DEFAULT_PORT;   //服务器端口号
+	struct sockaddr_in ser, cli;   //服务器地址，客户端地址
 	int iLen;
 
 	printf("-----------------------\n");
 	printf("Server waiting\n");
 	printf("-----------------------\n");
 
-	//��һ��������Э��ջ
+	//第一步：加载协议栈
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
 		printf("Failed to load Winsock.\n");
 		return USER_ERROR;
 	}
 
-	//�ڶ��������������׽��֣����ڼ����ͻ�����
+	//第二步：创建监听套接字，用于监听客户请求
 	sListen = socket(AF_INET, SOCK_STREAM, 0);
 	if (sListen == INVALID_SOCKET)
 	{
@@ -238,27 +238,27 @@ int main()
 		return USER_ERROR;
 	}
 
-	//������������ַ��IP+�˿ں�
+	//创建服务器地址：IP+端口号
 	ser.sin_family = AF_INET;
-	ser.sin_port = htons(serverport);               //�������˿ں�
-	ser.sin_addr.s_addr = htonl(INADDR_ANY);   //������IP��ַ��Ĭ��ʹ�ñ���IP
+	ser.sin_port = htons(serverport);               //服务器端口号
+	ser.sin_addr.s_addr = htonl(INADDR_ANY);   //服务器IP地址，默认使用本机IP
 
-	//���������󶨼����׽��ֺͷ�������ַ
+	//第三步：绑定监听套接字和服务器地址
 	if (bind(sListen, (LPSOCKADDR)&ser, sizeof(ser)) == SOCKET_ERROR)
 	{
 		printf("blind() Failed:%d\n", WSAGetLastError());
 		return USER_ERROR;
 	}
 
-	//���岽��ͨ�������׽��ֽ��м���
+	//第五步：通过监听套接字进行监听
 	if (listen(sListen, 5) == SOCKET_ERROR)
 	{
 		printf("listen() Failed:%d\n", WSAGetLastError());
 		return USER_ERROR;
 	}
-	while (1)  //ѭ���ȴ��ͻ�������
+	while (1)  //循环等待客户的请求
 	{
-		//�����������ܿͻ��˵��������󣬷�����ÿͻ������������׽���
+		//第六步：接受客户端的连接请求，返回与该客户建立的连接套接字
 		iLen = sizeof(cli);
 		sAccept = accept(sListen, (struct sockaddr*)&cli, &iLen);
 		if (sAccept == INVALID_SOCKET)
@@ -266,7 +266,7 @@ int main()
 			printf("accept() Failed:%d\n", WSAGetLastError());
 			break;
 		}
-		//���߲��������߳̽������������
+		//第七步，创建线程接受浏览器请求
 		DWORD ThreadID;
 		CreateThread(NULL, 0, SimpleHTTPServer, (LPVOID)sAccept, 0, &ThreadID);
 	}
