4c39ce9cc965d5b62d9ae80b759e429c4cb959f8
Zichen Luo <iluo@163.com>
2019-05-07 03:51:14 +0800

    Create client.h

---
+++ Assignment2/Chat/Chat_Client/client.h
@@ -0,0 +1,26 @@
+//
+// Created by rainy on 5/7/2019.
+//
+
+#ifndef CHAT_CLIENT_CLIENT_H
+#define CHAT_CLIENT_CLIENT_H
+
+
+// char userName[16] = {0};
+
+unsigned __stdcall ThreadRecv(void *param);
+
+unsigned __stdcall ThreadSend(void *param);
+
+int genKey();
+
+int sayHello(SOCKET ClientSocket);
+
+int handShaking(SOCKET ClientSocket);
+
+int ConnectChatServer();
+
+int main();
+
+
+#endif //CHAT_CLIENT_CLIENT_H
