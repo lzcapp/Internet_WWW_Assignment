f1915003f7c40b01904a21e51fef9c345e76032a
Zichen Luo <iluo@163.com>
2019-05-07 11:44:16 +0800

    Update client.cpp

--- Assignment2/Chat/Chat_Client/client.cpp
+++ Assignment2/Chat/Chat_Client/client.cpp
@@ -4,13 +4,16 @@
 #include <cstdlib>
 #include <conio.h>
 
-// #include "DES.h"
-#include "DES.cpp"
+// #include "des.h"
+#include "des.cpp"
+#include "client.h"
 
 #pragma comment(lib, "ws2_32.lib")
 
 #pragma warning (disable: 4996)
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnusedValue"
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCDFAInspection"
 #pragma clang diagnostic push
@@ -23,20 +26,69 @@
 // #define RECV_OVER 1
 // #define RECV_YET 0
 
-char userName[16] = {0};
+char myName[128] = {0};
+char urName[128] = {0};
 // int iStatus = RECV_YET;
 
 
 unsigned __stdcall ThreadRecv(void *param) {
-    char buf[128] = {0};
+    char buf[262144];
+    auto ClientSocket = *(SOCKET *) param;
     while (true) {
-        int ret = recv(*(SOCKET *) param, buf, sizeof(buf), 0);
+        int ret = recv(ClientSocket, buf, sizeof(buf), 0);
         if (ret == SOCKET_ERROR) {
             Sleep(500);
             continue;
         }
         if (strlen(buf) != 0) {
+            char head[10] = {0};
+            strncpy(head, buf, 4);
+            if (strcmp(head, "NAME") == 0) {
+                strcpy(urName, buf);
+                char msg[128] = {0};
+                strcpy(msg, "NAME: ");
+                strcat(msg, myName);
+                int sta = send(ClientSocket, msg, sizeof(msg), 0);
+                if (sta == SOCKET_ERROR) {
+                    return 1;
+                }
+            } else if (strcmp(head, "METH") == 0) {
+                strcpy(urName, buf);
+                char msg[128];
+                strcpy(msg, "METHOD: DES");
+                int sta = send(ClientSocket, msg, sizeof(msg), 0);
+                if (sta == SOCKET_ERROR) {
+                    return 1;
+                }
+            } else if (strcmp(head, "TEST") == 0) {
+                char testCipher[128];
+                ret = recv(ClientSocket, testCipher, sizeof(testCipher), 0);
+                if (ret == SOCKET_ERROR) {
+                    return -1;
+                }
+                if (strlen(testCipher) != 0) {
+                    string tmp;
+                    tmp = testCipher;
+                    tmp = tmp.substr(5);
+                    char *p = const_cast<char *>(tmp.data());
+                    msg_decryption(p);
+                    if (msg_result == "TEST_MSG_DES") {
+                        printf("Connection Successful!\n");
+                    }
+                    char msg[128];
+                    strcpy(msg, "TEST_MSG_DES");
+                    int sta = send(ClientSocket, msg, sizeof(msg), 0);
+                    if (sta == SOCKET_ERROR) {
+                        return 1;
+                    }
+                } else {
+                    return -1;
+                }
+            }
             printf("%s\n", buf);
+            msg_result = "";
+            msg_decryption(buf);
+            std::cout << msg_result << std::endl;
             // iStatus = RECV_OVER;
         } else {
             Sleep(100);
@@ -54,9 +106,16 @@ unsigned __stdcall ThreadSend(void *param) {
         if (c == 72 || c == 0 || c == 68) {
             continue;
         }
-        printf("%s: ", userName);
+        printf("%s: ", myName);
         gets(buf);
-        ret = send(*(SOCKET *) param, buf, sizeof(buf), 0);
+        printf("\n");
+        printf(buf);
+        msg_result = "";
+        msg_encryption(buf);
+        std::cout << msg_result << std::endl;
+        char* msg = (char *)msg_result.c_str();
+        printf("%d\n", msg_result.length());
+        ret = send(*(SOCKET *) param, msg, msg_result.length(), 0);
         if (ret == SOCKET_ERROR) {
             return 1;
         }
@@ -88,6 +147,7 @@ int ConnectChatServer() {
     }
 
     char IP[32] = {0};
+    // Server at localhost 127.0.0.1
     strcpy(IP, "127.0.0.1");
 
     ServerAddr.sin_family = AF_INET;
@@ -102,12 +162,12 @@ int ConnectChatServer() {
         WSACleanup();
         return -1;
     }
-    printf("Connecting server successfully IP:%s Port:%d\n", IP, htons(ServerAddr.sin_port));
-    printf("Please input your UserName: ");
-    gets(userName);
-    send(ClientSocket, userName, sizeof(userName), 0);
-    printf("\n");
-    _beginthreadex(nullptr, 0, ThreadRecv, &ClientSocket, 0, nullptr); //启动接收和发送消息线程
+    printf("Connect to chat server successfully.\n");
+    printf("\t=> IP: %s ,Port: %d\n\n", IP, htons(ServerAddr.sin_port));
+    printf("My name is: ");
+    gets(myName);
+    sayHello(ClientSocket);
+    _beginthreadex(nullptr, 0, ThreadRecv, &ClientSocket, 0, nullptr);
     _beginthreadex(nullptr, 0, ThreadSend, &ClientSocket, 0, nullptr);
     for (int k = 0; k < 1000; k++) {
         Sleep(10000000);
@@ -117,11 +177,89 @@ int ConnectChatServer() {
     return 0;
 }
 
+int sayHello(SOCKET ClientSocket) {
+    char msg[128];
+    strcpy(msg, "NAME: ");
+    strcat(msg, myName);
+    int sta = send(ClientSocket, msg, sizeof(msg), 0);
+    if (sta == SOCKET_ERROR) {
+        return 1;
+    }
+    char buf[128];
+    int ret = recv(ClientSocket, buf, sizeof(buf), 0);
+    if (ret == SOCKET_ERROR) {
+        return -1;
+    }
+    if (buf[0] == 'N') {
+        strcpy(urName, buf);
+    } else {
+        return -1;
+    }
+    handShaking(ClientSocket);
+}
+
+int handShaking(SOCKET ClientSocket) {
+    char msg[128];
+    strcpy(msg, "METHOD: DES");
+    int sta = send(ClientSocket, msg, sizeof(msg), 0);
+    if (sta == SOCKET_ERROR) {
+        return 1;
+    }
+    char buf[128];
+    int ret = recv(ClientSocket, buf, sizeof(buf), 0);
+    if (ret == SOCKET_ERROR) {
+        return -1;
+    }
+    if (strlen(buf) != 0) {
+        if (strcmp(buf, "METHOD: DES") == 0) {
+            // genKey();
+            // char key_send[128];
+            // strcat(key_send, "KEY: ");
+            // char *key = (char *) k.c_str();
+            // strcat(key_send, key);
+            // sta = send(ClientSocket, msg, sizeof(msg), 0);
+            // if (sta == SOCKET_ERROR) {
+            //    return 1;
+            // }
+            char temp_msg[1024] = {0};
+            strcpy(temp_msg, "TEST_MSG_DES");
+            msg_encryption(temp_msg);
+            strcpy(msg, "TEST: ");
+            strcat(msg, (char *)msg_result.c_str());
+            int sta = send(ClientSocket, msg, sizeof(msg), 0);
+            if (sta == SOCKET_ERROR) {
+                return 1;
+            }
+            char testCipher[128];
+            ret = recv(ClientSocket, testCipher, sizeof(testCipher), 0);
+            if (ret == SOCKET_ERROR) {
+                return -1;
+            }
+            if (strlen(testCipher) != 0) {
+                if (strcmp(testCipher, "TEST_MSG_DES") == 0) {
+                    printf("Connection Successful!\n");
+                }
+            } else {
+                return -1;
+            }
+        }
+    } else {
+        return -1;
+    }
+}
+
 int main() {
     ConnectChatServer();
+    /*char msg[1024];
+    strcpy(msg, "caonima");
+    msg_encryption(msg);
+    strcpy(msg, "813DB7B34102D5E2");
+    msg_decryption(msg);*/
     return 0;
 }
 
+// #pragma clang diagnostic pop //
+#pragma clang diagnostic pop
 #pragma clang diagnostic pop
 #pragma clang diagnostic pop
 #pragma clang diagnostic pop
